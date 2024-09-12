70b912fb4c930c6081a0eb0e425484df2025b90e
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-09-12 13:02:55 -0700

    Number Game

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+// pElement gets the user input, button submits your answer
 const pElement = document.getElementById("ansText");
 const button = document.getElementById("submitanswer");
-const y = Math.floor(Math.random() * 10 + 1);
+const y = Math.floor(Math.random() * 25 + 1);
 
+// If you click the submit button, it will activate the code below
 button.addEventListener("click", changeWords);
 
+// You will get your answer from here based on if your answer is true or false
 function changeWords(){
     
-    let x = parseInt(document.getElementById("guessbox").value); // Integer
+    // This changes the x into a whole number
+    let x = parseInt(document.getElementById("guessbox").value); 
      
     if(x > y){
         pElement.innerText = "Too High! Go Lower!";
+        pElement.style.color = "red";
     }
     else if(x === y){
         pElement.innerText = "You're Winner!";
+        pElement.style.color = "green";
     }
     else{
-        pElement.innerText = "Too Low! Go Higher!"
+        pElement.innerText = "Too Low! Go Higher!";
+        pElement.style.color = "red";
     }
 }
 
